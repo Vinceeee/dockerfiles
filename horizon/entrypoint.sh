392198c1a399bd80d1b679e530c7395398f96c9f
Vinceeee <491323832@qq.com>
2018-09-02 22:34:38 +0800

    add horizon

--- horizon/entrypoint.sh
+++ horizon/entrypoint.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env sh
 
-# build up mysql configuration at first
 
-if [[ -z $MYSQL_CONF ]];then
-    echo "No MYSQL-CONF, failed to config, please specify MYSQL_CONF as enviornmental variable"
-    exit 8
-fi
+/opt/horizon/manage.py compilemessages
+/opt/horizon/manage.py collectstatic
+/opt/horizon/manage.py compress --force
+/opt/horizon/manage.py make_web_conf --wsgi
 
 
-keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
-keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
-keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
- --bootstrap-admin-url http://192.168.56.106:5000/v3/ \
- --bootstrap-internal-url http://192.168.56.106:5000/v3/ \
- --bootstrap-public-url http://192.168.56.106:5000/v3/ \
- --bootstrap-region-id RegionOne
+exec "$@"
