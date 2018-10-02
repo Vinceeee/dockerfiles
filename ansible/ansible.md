817d408f9c30fa6b4a895aa3fcf0d34469aa738e
Vinceeee <491323832@qq.com>
2018-10-02 16:44:14 +0800

    add registry

--- ansible/ansible.md
+++ ansible/ansible.md
@@ -2,7 +2,7 @@
 
 #### 设置密码登录并执行相关操作:
 1. 在/etc/ansible/hosts中指定用户账号密码
-2. 在/ec/ansible/ansible.cfg中将host_key_check 设为 False
+2. 在/etc/ansible/ansible.cfg中将host_key_check 设为 False
 
 #### 使用sudo执行安装yum
 > ansible cluster -m yum -a "name=vim state=latest" --sudo
---
+++ registry/setup_registry.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+
+# create cerificate
+function create_certs() {
+    mkdir certs
+    openssl req -newkey rsa:2048 -nodes -sha256 -keyout certs/domain.key -x509 -days 1000 -out certs/domain.crt
+    exit 0
+}
+
+# make auth 
+function make_auth() {
+    mkdir auth
+    docker run --entrypoint htpasswd registry:2 -Bbn admin 123456 > auth/htpasswd
+}
+
+# run registry
+function create_registry() {
+    docker run -d -p 5000:5000 --restart=always --name registry \
+        -v `pwd`/auth:/auth \
+        -e "REGISTRY_AUTH=htpasswd" \
+        -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+        -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+        -v `pwd`/data:/var/lib/registry \
+        -v `pwd`/certs:/certs \
+        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+        -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+        registry:2
+}
+
+# 
+function grant_self_auth() {
+    fqdn="my.registry.com"
+    sudo mkdir -p /etc/docker/certs.d/$fqdn:5000
+    sudo cp certs/domain.crt /etc/docker/certs.d/$fqdn:5000/ca.crt
+    sudo systemctl restart docker
+}
+
+#create_certs
+#make_auth
+#create_registry
+grant_self_auth
