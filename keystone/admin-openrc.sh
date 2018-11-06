f6719717068ebb2326973ce169e4eaf637199f72
Vinceeeee <491323832@qq.com>
2018-11-06 22:28:12 +0800

    merge

--- keystone/admin-openrc.sh
+++ keystone/admin-openrc.sh
@@ -3,5 +3,5 @@ export OS_PASSWORD=ADMIN_PASS
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_DOMAIN_NAME=Default
-export OS_AUTH_URL=http://192.168.10.106:5000/v3
+export OS_AUTH_URL=http://127.0.0.1:5000/v3
 export OS_IDENTITY_API_VERSION=3
--- mariadb/utils.sh
+++ mariadb/utils.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-function create_keystone
-{
-    $KEYSTONE_DBPASSWD=$1
-    $KEYSTONE_DBPASSWD=$1
-    mysql -u root -e"CREATE DATABASE keystone if not exists;"
-    mysql -u root -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'$BIND_IP' IDENTIFIED BY '$KEYSTONE_DBPASSWD';"
+function create_keystone() {
+    $KEYSTONE_BINDIP=$1
+    $KEYSTONE_DBPASSWD=$2
+    mysql -u root  -e"CREATE DATABASE keystone if not exists;"
+    mysql -u root  -e"GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'$KEYSTONE_BINDIP' IDENTIFIED BY '$KEYSTONE_DBPASSWD';"
 }
 
-create_keystone $1
+if [[ $# -ne 2 ]]; then
+    echo cao
+    exit 1
+fi
+
+create_keystone $1 $2
