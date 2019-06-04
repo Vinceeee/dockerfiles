b037412d584110c3afec44043aad34f123626397
Vinceeee <491323832@qq.com>
2019-06-04 22:39:09 +0800

    add swift saio compose

--- compose/keystone-swift/swift/bin/entrypoint.sh
+++ compose/keystone-swift/swift/bin/entrypoint.sh
@@ -5,8 +5,19 @@ set -e
 # run this image locally:
 # docker run -d  -e SWIFT_LOOP=2G -e SWIFT_REPLICAS=2 --privileged --network=host --name=swift --hostname=swift1 -v /sys/fs/cgroup:/sys/fs/cgroup:ro
 
+function entrypoint_for_saio() {
+    devices=$SWIFT_DEVICES
+    OLD_IFS=$IFS
+    IFS=,
+    mkdir -p /srv/node
+    for dev in $devices;do
+        mkdir -p /srv/node/$dev
+    done
+    IFS=$OLD_IFS
+    chown -R swift:swift /var/cache /srv/node /etc/swift
+}
+
 function entrypoint_for_storage() {
-    #function_body
     devices=$SWIFT_DEVICES
     OLD_IFS=$IFS
     IFS=,
@@ -21,7 +32,6 @@ function entrypoint_for_storage() {
 }
 
 function entrypoint_for_proxy() {
-    #function_body
     cd /etc/swift
     rm -rf account* container* object*
     chown -R swift:swift /var/cache  /etc/swift
@@ -34,7 +44,6 @@ sed -i "s?swift_dir = .*?swift_dir = ${RING_DIR}?g" /etc/swift/*.conf
 mkdir -p /var/cache/swift
 
 echo "working on $SWIFT_TYPE initialization"
-#swift-init all start
 entrypoint_for_$SWIFT_TYPE
 
 exec "$@"
